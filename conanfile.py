ef9f7426fb7c9f572be8669ab7d8d035fdd40030
Christian Haege <kischy@gmx.de>
2024-04-22 22:09:05 +0200

    Added basic project sceleton

---
+++ conanfile.py
@@ -0,0 +1,13 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+
+
+class P38Euler(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        self.requires("gtest/1.14.0")
+
+    def layout(self):
+        cmake_layout(self)
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, from p38-euler!\n";
+    return 0;
+}
